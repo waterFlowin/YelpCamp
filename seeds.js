d8f49cb3d9f19e2627e67800bf44541ff341b7fb
coyote08 <c.swafford08@gmail.com>
2017-02-19 07:36:56 +0000

    initial deploy commit

---
+++ seeds.js
@@ -0,0 +1,58 @@
+var mongoose   = require("mongoose");
+var Campground = require("./models/campground");
+var Comment    = require("./models/comment");
+
+var data = [
+    {
+        name: "Cloud's Rest",
+        image: "https://farm8.staticflickr.com/7399/9211397472_e6d0a7c6fd.jpg",
+        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    },
+    {
+        name: "Desert Mesa",
+        image: "https://farm5.staticflickr.com/4017/4358461547_195ba4f6af.jpg",
+        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    },
+    {
+        name: "Canyon Floor",
+        image: "https://farm8.staticflickr.com/7399/9211397472_e6d0a7c6fd.jpg",
+        description: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+    }
+]
+
+function seedDB(){
+    //Remove all campgrounds
+    Campground.remove({}, function(err){
+        if(err){
+            console.log(err);
+        }
+        console.log("removed campgrounds!");
+        //add a few campgrounds
+        data.forEach(function(seed){
+            Campground.create(seed, function(err, campground){
+              if(err){
+                  console.log(err);
+              } else {
+                  console.log("added a campground");
+                  //create a comment
+                  Comment.create(
+                      {
+                        text: "This place is great but I wish there was internet",
+                        author: "Homer"
+                  }, function(err, comment){
+                      if(err){
+                          console.log(err);
+                      } else {
+                        campground.comments.push(comment);
+                        campground.save();
+                        console.log("Created new comment");
+                      }
+                  });
+              }
+            }); 
+        });
+    });
+    //add a few comments
+}
+
+module.exports = seedDB;
